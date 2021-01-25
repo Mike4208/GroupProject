c1ca63bdce9149a3108bf795fa988b23767db841
Anepsios <Anepsios@users.noreply.github.com>
2021-01-25 21:59:25 +0200

    -comment out ratingviewmodel

--- GroupProject/ViewModel/RatingViewModel.cs
+++ GroupProject/ViewModel/RatingViewModel.cs
@@ -5,15 +5,15 @@ using System.Web;
 
 namespace GroupProject.ViewModel
 {
-    public class RatingViewModel
-    {
-        public int RatingID { get; set; }
-        public int ProductID { get; set; }
-        public string RatingText { get; set; }
-        public string Username { get; set; }
-        public string Name { get; set; }
-        public bool IsApproved { get; set; }
-        public DateTime ReviewCreated { get; set; }
-        public int Stars { get; set; }
-    }
+    //public class RatingViewModel
+    //{
+    //    public int RatingID { get; set; }
+    //    public int ProductID { get; set; }
+    //    public string RatingText { get; set; }
+    //    public string Username { get; set; }
+    //    public string Name { get; set; }
+    //    public bool IsApproved { get; set; }
+    //    public DateTime ReviewCreated { get; set; }
+    //    public int Stars { get; set; }
+    //}
 }
